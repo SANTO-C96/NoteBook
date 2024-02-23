9092fa7e0d8540f2d5ad51aa05394e456578b6d1
SANTO-C96 <89244724+SANTO-C96@users.noreply.github.com>
2024-02-23 18:41:30 +0530

    Update index.js
    updates in the cors orgin

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,13 @@ const app = express();
 
 connectToMongo();
 const port = 5000;
-app.use(cors());
+app.use(cors(
+   {
+      origin:[],
+      methods: ["POST","GET"],
+      credentials: true
+   }
+));
 app.use(express.json());
 app.use('/api/auth', require('./routes/auth'))
 app.use('/api/notes', require('./routes/notes'))
